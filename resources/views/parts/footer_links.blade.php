1869f15005bccddb1bfe92bb3d59f38b6626a5f3
Dmitrii Iurev <Dmitrii_Iurev@epam.com>
2017-01-17 17:18:34 +0400

    Footer links and info actualized

--- resources/views/parts/footer_links.blade.php
+++ resources/views/parts/footer_links.blade.php
@@ -17,11 +17,10 @@
                 <h6>Навигация</h6>
                 <hr>
                 <ul class="navigation">
-                    <li> <a href="index.html">Домашняя</a> </li>
-                    <li> <a href="post.html">Новости</a> </li>
-                    <li> <a href="elements.html">Статьи</a> </li>
-                    <li> <a href="about-us.html">Обо мне</a> </li>
-                    <li> <a href="contact.html">Обратная связь</a> </li>
+                    <li> <a href="{{ route('site.main.index') }}">Главная</a> </li>
+                    <li> <a href="{{ route('site.main.about') }}">Кто я?</a> </li>
+                    <li> <a href="{{ route('site.main.feedback') }}">Написать мне</a> </li>
+                    <li> <a href="{{ route('site.auth.register') }}">Регистрация</a> </li>
                 </ul>
             </nav>
         </div>
@@ -30,10 +29,10 @@
                 <h6>Контакты</h6>
                 <hr>
                 <span class="widget-contact__text">
-                    <span class="widget-contact__title">Иванов Иван Иванович</span>
-                    <br>Email: ivan@ivanov.ru
-                    <br>Skype: ivanovivan
-                    <br>VK: https://vk.com/ivanovoficial
+                    <span class="widget-contact__title">Юрьев Дмитрий Сергеевич</span>
+                    <br>Email: dima@932433.ru
+                    <br>Skype: dyus84
+                    <br>VK: https://vk.com/dyuryev84
                     </span>
             </div>
         </div>
